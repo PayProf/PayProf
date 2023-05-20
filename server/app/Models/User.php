6879c8a5fccfaf648f78a04c19c45f85c22c318c
imads544 <imadsamadi971@gmail.com>
2023-05-20 19:52:51 +0100

    added login and logout

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -31,6 +31,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'email_verified_at',
+        'created_at',
+        'updated_at',
+        'email',
+        'id',
     ];
 
     /**
--- server/routes/api.php
+++ server/routes/api.php
@@ -8,17 +8,9 @@ use App\Http\Controllers\api\InterventionController;
 use App\Http\Controllers\api\PaiementsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 route::apiResource('ens',EnseignantController::class);
 //route of  ShowMyInterventions
 route::get('ens/{ens}/inter',[EnseignantController::class,'ShowMyInterventions']);
@@ -34,6 +26,15 @@ route::apiResource('int',InterventionController::class);
 route::apiResource('pay',PaiementsController::class);
 
 route::apiResource('etab',EtablissementController::class);
+route::post('/login',[AuthController::class,'login']);
+
+route::group(['middleware'=>['auth:sanctum']],function(){
+    route::post('/logout',[AuthController::class,'logout']);
+    
+}
+);
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
